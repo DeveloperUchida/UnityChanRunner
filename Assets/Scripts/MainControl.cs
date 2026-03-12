244b18fc662aa885ba15940410881c6223827a7a
DeveloperUchida <kouei2257@gmail.com>
2026-03-12 13:58:08 +0900

    Edit:歩行機能およびモーション動作機能を実装

--- Assets/Scripts/MainControl.cs
+++ Assets/Scripts/MainControl.cs
@@ -1,36 +1,57 @@
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+///<summary>
+/// Untiyちゃんの移動関係をコントロールしているプログラム
+///</summary>
 
 public class MainControl : MonoBehaviour
 {
+    private Animator amimator;
+    private new Rigidbody rigidbody;
+    /// <summary>
+    /// 移動関数関係
+    /// </summary>
+    public float moveSpeed = 3;
+    public float jumpForce = 5;
+    private bool isGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        amimator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //3DはZが存在するからそこを前進として考えよう
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(0, 0, 0);
-        }
+        float h = Input.GetAxis("Horizontal"); //A/D
+        float v = Input.GetAxis("Vertical"); //W/S
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0, 0, 0);
-        }
+        //移動処理
+        Vector3 move = new Vector3(h, 0, v) * moveSpeed * Time.deltaTime;
+        transform.Translate(move);
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0, 0, 0);
-        }
+        //アニメーション制御
 
-        if (Input.GetKey(KeyCode.D))
+        float Speed = new Vector2(h, v).magnitude;
+        amimator.SetFloat("Speed", Speed);
+        amimator.SetFloat("Direction", h);
+
+        //ジャンプ操作
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
-            transform.Rotate(0, 0, 0);
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            amimator.SetBool("jump", true);
+            isGround = false;
         }
-        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+            amimator.SetBool("jump", false);
+        }
     }
 }
